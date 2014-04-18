775789bd65c59b5892a058feaadc3d9f188eb2e6
chunksahoy <charles_hunter182@hotmail.com>
2014-04-18 13:54:33 -0400

    Projet mostly done

---
+++ src/javaapplication3/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication3;
+
+/**
+ *
+ * @author 200777439
+ */
+public class Main {
+    public static void main(String[] args) {
+      // cette classe appelle la fênetre d'identifition.      
+        Identifier fenetrePrincipale = new Identifier();
+        fenetrePrincipale.setVisible(true);
+   }    
+}
